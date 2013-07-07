6522b4becfe5b7b57c2ab9c91ce735c1e3ef61ae
Christopher Roy Bratusek <nano@tuxfamily.org>
2013-07-07 08:40:20 +0200

    save game hack for new super mario bros. 4

--- script.d/nsmbw4.sh
+++ script.d/nsmbw4.sh
@@ -95,6 +95,10 @@ place_files () {
 
 dolpatch () {
 
-	${WIT} dolpatch ${DOL} xml="patches/NSMBW_AP.xml" -q
+	${WIT} dolpatch ${DOL}	\
+		"802F148C=53756D6D53756E#7769696D6A3264" \
+		"802F118C=53756D6D53756E#7769696D6A3264" \
+		"802F0F8C=53756D6D53756E#7769696D6A3264" \
+		xml="patches/NSMBW_AP.xml" -q
 
 }
