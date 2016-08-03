be2b5d11691c1a82eabe1075c6a5396c4f15b815
Christopher Roy Bratusek <nano@jpberlin.de>
2016-08-03 22:15:03 +0200

    add support for Revised Super Mario Bros. Wii

---
+++ script.d/revisedsupermariobroswii.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+WORKDIR=nsmb.d
+DOL=${WORKDIR}/sys/main.dol
+DOWNLOAD_LINK="https://www.dropbox.com/s/pdxfqfvd19lema3/RSMBW%20Version%201.0.zip?dl=0"
+RIIVOLUTION_ZIP="RSMBW Version 1.0.zip"
+RIIVOLUTION_DIR="RSMBW Version 1.0/rsmbw"
+GAMENAME="Revised Super Mario Bros. Wii"
+XML_SOURCE="${RIIVOLUTION_DIR}/"
+XML_FILE="${RIIVOLUTION_DIR}/../riivolution/RSMBW.xml"
+GAME_TYPE=RIIVOLUTION
+BANNER_LOCATION=${WORKDIR}/files/opening.bnr
+
+show_notes () {
+
+echo -e \
+"************************************************
+${GAMENAME}
+
+Source:			http://rvlution.net/thread/4579-revised-super-mario-bros-wii/
+Base Image:		New Super Mario Bros. Wii (SMN?01)
+Supported Versions:	EURv1, EURv2, USAv1, USAv2, JPNv1
+************************************************"
+
+}
+
+check_input_image_special () {
+
+	check_input_image_nsmb
+
+}
+
+detect_game_version () {
+
+	nsmbw_version
+	GAMEID=SMN${REG_LETTER}69
+}
+
+place_files () {
+
+	NEW_DIRS=( ${WORKDIR}/files/EU/NedEU/{Message,Font,staffroll} )
+	for dir in ${NEW_DIRS[@]}; do
+		mkdir -p ${dir}
+	done
+
+	case ${VERSION} in
+		EUR* )
+			LANGDIRS=( EngEU FraEU GerEU ItaEU SpaEU NedEU )
+			for dir in ${LANGDIRS[@]}; do
+				cp "${RIIVOLUTION_DIR}"/Lang/EUENGLISH.arc ${WORKDIR}/files/EU/${dir}/Message/Message.arc
+			#	cp "${RIIVOLUTION_DIR}"/staffroll/staffroll.bin ${WORKDIR}/files/EU/${dir}/staffroll/
+			done
+			#cp "${RIIVOLUTION_DIR}"/Layout/openingTitle.arc ${WORKDIR}/files/EU/Layout/openingTitle/
+		;;
+
+		USAv* )
+			LANGDIRS=( FraUS EngUS SpaUS )
+			for dir in ${LANGDIRS[@]}; do
+				cp "${RIIVOLUTION_DIR}"/Lang/USENGLISH.arc ${WORKDIR}/files/US/${dir}/Message/Message.arc
+				cp "${RIIVOLUTION_DIR}"/staffroll/staffroll.bin ${WORKDIR}/files/US/${dir}/staffroll/
+			done
+			cp "${RIIVOLUTION_DIR}"/Layout/openingTitle.arc ${WORKDIR}/files/US/Layout/openingTitle/
+		;;
+
+		JPNv1 )
+			#cp "${RIIVOLUTION_DIR}"/Lang/URA-JAPANESE.arc ${WORKDIR}/files/JP/Message/Message.arc
+			cp "${RIIVOLUTION_DIR}"/Layout/openingTitle.arc ${WORKDIR}/files/JP/Layout/openingTitle/
+			#cp "${RIIVOLUTION_DIR}"/staffroll/staffroll.bin ${WORKDIR}/files/JP/staffroll/
+		;;
+	esac
+
+	cp -r "${RIIVOLUTION_DIR}"/Object/ ${WORKDIR}/files/
+	cp "${RIIVOLUTION_DIR}"/Sound/*.brsar ${WORKDIR}/files/Sound/
+	cp "${RIIVOLUTION_DIR}"/Stage/Texture/* ${WORKDIR}/files/Stage/Texture/
+	cp "${RIIVOLUTION_DIR}"/Stage/*.arc ${WORKDIR}/files/Stage/
+
+}
+
+dolpatch () {
+
+	cp "${XML_FILE}" "${XML_FILE}".new
+	sed -e 's/80001800/803482C0/g' -i "${XML_FILE}".new
+	XML_FILE="${XML_FILE}".new
+
+	#${WIT} dolpatch ${DOL} xml="${XML_FILE}" -s "${XML_SOURCE}" \
+	#	xml="${PATCHIMAGE_PATCH_DIR}/NewerSMBW-Loader.xml" -q
+	${WIT} dolpatch ${DOL} xml="${PATCHIMAGE_PATCH_DIR}/NSMBW_AP.xml" -q
+
+}
