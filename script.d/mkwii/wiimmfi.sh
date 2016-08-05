6092d3f7410b08f870217e72e364ed6bc01c5bfe
Christopher Roy Bratusek <nano@jpberlin.de>
2016-08-05 21:15:45 +0200

    wiimmfi.sh: fixes and code review

--- script.d/mkwii/wiimmfi.sh
+++ script.d/mkwii/wiimmfi.sh
@@ -22,13 +22,13 @@ Supported Versions:	EUR, JAP, USA
 check_input_image_special () {
 
 	ask_input_image_mkwiimm
-	echo -e "type ALL or RMC???.wbfs:\n"
-	read ID
+	echo -e "type RMC???.wbfs:\n"
+	read -r ID
 
 	if [[ -f ${PWD}/${ID} ]]; then
-		GAMEDIR=${PWD}
+		GAMEDIR="${PWD}"
 	elif [[ -f ${PATCHIMAGE_WBFS_DIR}/${ID} ]]; then
-		GAMEDIR=${PATCHIMAGE_WBFS_DIR}
+		GAMEDIR="${PATCHIMAGE_WBFS_DIR}"
 	else	echo "invalid user input."
 		exit 75
 	fi
@@ -39,43 +39,26 @@ check_input_image_special () {
 download_wiimm () {
 
 	mkdir -p "${HOME}/.patchimage/tools/"
-	cd ${HOME}/.patchimage/tools
-	rm -rf wiimmfi-patcher/ *.7z*
+	cd "${HOME}"/.patchimage/tools
+	rm -rf wiimmfi-patcher/ ./*.7z*
 	wget "${DOWNLOAD_LINK}" || ( echo "something went wrong downloading ${DOWNLOAD_LINK}" && exit 57 )
-	${UNP} mkw-wiimmfi-patcher.7z >/dev/null ( echo "something went wrong unpacking files" && exit 63 )
+	"${UNP}" mkw-wiimmfi-patcher.7z >/dev/null || ( echo "something went wrong unpacking files" && exit 63 )
 	mv mkw-wiimmfi-patcher*/ wiimmfi-patcher
 	chmod +x wiimmfi-patcher/*.sh
-	rm *.7z
+	rm ./*.7z
+
 }
 
 patch_wiimm () {
 
-	cd ${HOME}/.patchimage/tools/wiimmfi-patcher/
+	cd "${HOME}"/.patchimage/tools/wiimmfi-patcher/
 
-	if [[ ${ID} == ALL ]]; then
-		for image in ${IMAGE%/*}/RMC???.{iso,wbfs}; do
-			if [[ -e ${image} ]]; then
-				ln -s "${image}" .
-			fi
-		done
+	ln -s "${GAMEDIR}"/"${ID}" .
+	./patch-wiimmfi.sh >/dev/null || \
+		( echo "wiimmfi-ing the image failed." && exit 69 )
+	mv -v ./wiimmfi-images/"${ID}" "${PATCHIMAGE_GAME_DIR}"/
 
-			./patch-wiimmfi.sh >/dev/null || \
-				( echo "wiimmfi-ing the images failed." && exit 69 )
-			mv -v ./wiimmfi-images/* "${PATCHIMAGE_GAME_DIR}"/
-
-	else
-		if [[ ! -f ${IMAGE%/*}/${ID} ]]; then
-			echo "unvalid game passed from user-input. exit"
-			exit 75
-		fi
-
-		ln -s ${IMAGE%/*}/${ID} .
-		./patch-wiimmfi.sh >/dev/null || \
-			( echo "wiimmfi-ing the image failed." && exit 69 )
-		mv -v ./wiimmfi-images/${ID} "${PATCHIMAGE_GAME_DIR}"/
-	fi
-
-	rm -rf ${HOME}/.patchimage/tools/wiimfi-patcher/
+	rm -rf "${HOME}"/.patchimage/tools/wiimfi-patcher/
 }
 
 pi_action () {
