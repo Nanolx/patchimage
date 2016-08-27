6b4d4a655f8c46b02d4f5ff8c43318116a1cfe24
Christopher Roy Bratusek <nano@jpberlin.de>
2016-08-27 10:51:55 +0200

    make downloader don't stop upon sendspace

--- scripts/common/downloader.sh
+++ scripts/common/downloader.sh
@@ -60,15 +60,6 @@ download_riivolution_patch () {
 			exit 21
 		;;
 
-		*sendspace* )
-			x=6
-			echo "can not download from SendSpace, download manually from:
-
-	${DOWNLOAD_LINK}
-"
-			exit 21
-		;;
-
 		*romhacking* )
 			x=6
 			echo "can not download from Romhacking, download manually from:
