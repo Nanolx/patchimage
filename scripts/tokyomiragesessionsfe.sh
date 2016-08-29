2e3da07726f09e7ce2676476bbec5b4400d1ab76
Christopher Roy Bratusek <nano@jpberlin.de>
2016-08-29 21:42:38 +0200

    fixup Tokyo Mirage Sessions FE patcher

--- scripts/tokyomiragesessionsfe.sh
+++ scripts/tokyomiragesessionsfe.sh
@@ -10,7 +10,7 @@ DOWNLOAD_LINKS=( "patch_000_map.xdelta+https://drive.google.com/uc?id=0BxykxdPQq
 		 "patch_030_etc.xdelta+https://drive.google.com/uc?id=0BxykxdPQq3oZNEZfN1hqOHVYZ0E&export=download"
 		 "patch_031_message.xdelta+https://drive.google.com/uc?id=0BxykxdPQq3oZTDlRdzlkUno3ZXM&export=download"
 		 "patch_050_movie.xdelta+https://drive.google.com/uc?id=0BxykxdPQq3oZNU5kNlh2d1VRQzQ&export=download"
-		 "patch_999_etc_om.xdelta+https://drive.google.com/drive/folders/0BxykxdPQq3oZbVNGODB2TFlKTHc"
+		 "patch_999_etc_om.xdelta+https://drive.google.com/uc?id=0BxykxdPQq3oZN2VxTERFY2JPUFU"
 		 "patch_999_lua.xdelta+https://drive.google.com/uc?id=0BxykxdPQq3oZUERRR0w2Nnl2YUk&export=download" )
 
 show_notes () {
@@ -58,13 +58,24 @@ menu () {
 		for delta in ${DOWNLOAD_LINKS[@]}; do
 			DL_NAME=${delta/+*}
 			DL_LINK=${delta/*+}
-			if [[ ! -f "${PATCHIMAGE_RIIVOLUTION_DIR}/TMSFE_Restoration/${DL_NAME}" ]]; then
-				${GDOWN} "${DL_LINK}" \
-					"${PATCHIMAGE_RIIVOLUTION_DIR}/TMSFE_Restoration/${DL_NAME}"__tmp || \
-					( rm "${PATCHIMAGE_RIIVOLUTION_DIR}/TMSFE_Restoration/${DL_NAME}"__tmp && \
-						echo -e "\nDownload failed!" && exit 57 )
-				mv "${PATCHIMAGE_RIIVOLUTION_DIR}/TMSFE_Restoration/${DL_NAME}"__tmp \
-					"${PATCHIMAGE_RIIVOLUTION_DIR}/TMSFE_Restoration/${DL_NAME}"
+			if [[ ${DL_NAME} == patch_999_etc_om.xdelta ]]; then
+				if [[ ! -f "${PATCHIMAGE_RIIVOLUTION_DIR}/TMSFE_Restoration/${DL_NAME}" ]]; then
+					wget -nv "${DL_LINK}" \
+						-O "${PATCHIMAGE_RIIVOLUTION_DIR}/TMSFE_Restoration/${DL_NAME}"__tmp || \
+						( rm "${PATCHIMAGE_RIIVOLUTION_DIR}/TMSFE_Restoration/${DL_NAME}"__tmp && \
+							echo -e "\nDownload failed!" && exit 57 )
+					mv "${PATCHIMAGE_RIIVOLUTION_DIR}/TMSFE_Restoration/${DL_NAME}"__tmp \
+						"${PATCHIMAGE_RIIVOLUTION_DIR}/TMSFE_Restoration/${DL_NAME}"
+				fi
+			else
+				if [[ ! -f "${PATCHIMAGE_RIIVOLUTION_DIR}/TMSFE_Restoration/${DL_NAME}" ]]; then
+					${GDOWN} "${DL_LINK}" \
+						"${PATCHIMAGE_RIIVOLUTION_DIR}/TMSFE_Restoration/${DL_NAME}"__tmp || \
+						( rm "${PATCHIMAGE_RIIVOLUTION_DIR}/TMSFE_Restoration/${DL_NAME}"__tmp && \
+							echo -e "\nDownload failed!" && exit 57 )
+					mv "${PATCHIMAGE_RIIVOLUTION_DIR}/TMSFE_Restoration/${DL_NAME}"__tmp \
+						"${PATCHIMAGE_RIIVOLUTION_DIR}/TMSFE_Restoration/${DL_NAME}"
+				fi
 			fi
 		done
 	fi
