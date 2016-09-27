23ad9ccc41071bdfd7266a2a01eda676ab3678c4
Christopher Roy Bratusek <nano@jpberlin.de>
2016-09-27 17:58:12 +0200

    ::game updates: - Pokemon Rising Ruby: support version 2.0 - Pokemon Sinking Sapphire: support version 2.0

--- scripts/pokemon/pokemonrisingruby.sh
+++ scripts/pokemon/pokemonrisingruby.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-DOWNLOAD_LINK="https://drive.google.com/uc?id=0BzJZMhlTQ-CPaUtiTlp4YjlEY3M&export=download"
-RIIVOLUTION_ZIP="RRSSFiles 18-11-2015.zip"
-RIIVOLUTION_DIR="RRSSFiles 18-11-2015"
+DOWNLOAD_LINK="https://drive.google.com/uc?id=0B-zmEVN0Mas6NUlqM1hMTnVuS00&export=download"
+RIIVOLUTION_ZIP="RRSSv2.0.zip"
+RIIVOLUTION_DIR="RRSSv2.0"
 GAMENAME="Pokemon Rising Ruby"
 GAME_TYPE=HANS
 
 ROM_MASK="*000400000011[cC]400*cxi"
 ROMFS="RisingRuby.romfs"
-DATA="${PATCHIMAGE_DATA_DIR}/RisingRuby/"
+DATA="${PATCHIMAGE_DATA_DIR}/Rising Ruby Files/Raw Files/romFS a/"
 
 show_notes () {
 
@@ -16,7 +16,7 @@ echo -e \
 "************************************************
 ${GAMENAME}
 
-Source:			https://twitter.com/Drayano60/status/662793363255656448
+Source:			https://projectpokemon.org/forums/showthread.php?49883-Pok%E9mon-Rising-Ruby-and-Sinking-Sapphire
 Base ROM:		Pokemon Omega Ruby
 Supported Versions:	US, EU, JAP
 ************************************************"
@@ -31,6 +31,6 @@ check_hans_files () {
 
 patch_romfs () {
 
-	cp -r "${RIIVOLUTION_DIR}"/a romfs/
+	cp -r "${RIIVOLUTION_DIR}"/* romfs/a/
 
 	}
